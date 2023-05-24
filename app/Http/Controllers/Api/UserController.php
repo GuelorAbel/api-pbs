804c93336a8157faf6991aee1447ad6b7aa6bb1e
Aby <gueloraby@gmail.com>
2023-05-24 22:42:48 +0100

    Finalisation de la création d'un utilisateur

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,17 +5,30 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\SignupUser;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function signup(SignupUser $request)
     {
-        $user = new User();
+        try{
 
-        // récupération des données saisies dans le formulaire
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->password = $request->password;
-        $user->save(); //action qui permet de créer l'utilisateur à la soumission du formulaire
+            $user = new User();
+
+            // récupération des données saisies dans le formulaire
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = Hash::make($request->password, [
+                'rounds' => 12
+            ]);
+            $user->save(); //action qui permet de créer l'utilisateur à la soumission du formulaire
+
+            return response()->json([
+                'status' => 200,
+                'message' => "Le compte associé à $user->name a été créé.",
+            ]);
+        }catch(Exception $e){
+            return response()->json($e);
+        }
     }
 }
